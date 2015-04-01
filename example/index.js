48ed7cd9f8aefcccda9c26f85c2c64b3b7c284e1
Montana Flynn <montana@montanaflynn.me>
2015-04-01 03:24:22 -0700

    Make usage examples more verbose

--- example/index.js
+++ example/index.js
@@ -1,12 +1,14 @@
 var harplayer = require('../harplayer.js')
 var har = require('./har.json')
 
-harplayer.replayOne(har, 0, function(e, r, b){
-	if (e) throw e
-	console.log(r.statusCode + " " + r.statusMessage)
+harplayer.replayOne(har, 0, function(err, res, body){
+	if (err) throw err
+	console.log(res.statusCode + " " + res.statusMessage)
+	console.log(body)
 })
 
-harplayer.replayAll(har, function(e, r, b){
-	if (e) throw e
-	console.log(r.statusCode + " " + r.statusMessage)
+harplayer.replayAll(har, function(err, res, body){
+	if (err) throw err
+	console.log(res.statusCode + " " + res.statusMessage)
+	console.log(body)
 })
