d345f51bfa06c85ed5a362bdf52bafcfa8d568be
Montana Flynn <montana@montanaflynn.me>
2015-04-03 03:12:59 -0700

    Add some tests for each method

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,4 @@
+var mocha = require('mocha')
 var assert = require('assert')
 var nock = require('nock')
 var harplayer = require('../harplayer')
@@ -9,23 +10,47 @@ nockServer.get('/redirect').reply(301).persist()
 nockServer.get('/fail').reply(501).persist()
 
 describe('replayOne', function(){
-  describe('responseCode', function(){
+  describe('responseCodes', function(){
     it('should return 200', function(done){
       harplayer.replayOne(har, 0, function(err, res, body){
-        assert.equal(res.statusCode, 200);
-        done();
+        assert.equal(res.statusCode, 200)
+        done()
       })
     })
     it('should return 301', function(done){
       harplayer.replayOne(har, 1, function(err, res, body){
-        assert.equal(res.statusCode, 301);
-        done();
+        assert.equal(res.statusCode, 301)
+        done()
       })
     })
     it('should return 501', function(done){
       harplayer.replayOne(har, 2, function(err, res, body){
-        assert.equal(res.statusCode, 501);
-        done();
+        assert.equal(res.statusCode, 501)
+        done()
+      })
+    })
+  })
+})
+
+describe('replayAllInParallel', function(){
+  describe('responseCodes', function(){
+    it('should return the correct amount', function(done){
+      var count = 0
+      harplayer.replayAllInParallel(har, function(err, res, body){
+        count++
+        if (count === 3) done()
+      })
+    })
+  })
+})
+
+describe('replayAll', function(){
+  describe('responseCodes', function(){
+    it('should return correct order', function(done){
+      var index = 0, codes = [200, 301, 501]
+      harplayer.replayAll(har, function(err, res, body){
+        assert.equal(res.statusCode, codes[index++])
+        if (index === codes.length) done()
       })
     })
   })
