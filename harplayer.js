926325792aa51e5153a705b73bce50154145ebb2
Montana Flynn <montana@montanaflynn.me>
2015-04-14 13:21:31 -0700

    Refactor code

--- harplayer.js
+++ harplayer.js
@@ -1,17 +1,5 @@
 var request = require('request')
 
-module.exports = {
-  replayOne: function(har, index, callback) {
-    replay(har.log.entries[index].request, callback)
-  }, 
-  replayAll: function(har, callback) {
-    oneByOne(har.log.entries, replay, callback)
-  }, 
-  replayAllInParallel: function(har, callback) {
-    allAtOnce(har.log.entries, replay, callback)
-  }
-}
-
 function replay(harr, callback) {
   var options = {
     method: harr.method,
@@ -25,10 +13,12 @@ function replay(harr, callback) {
 }
 
 function oneByOne(arr, fun, callback) {
-  var entries = cloneArray(arr)
+  var entries = arr.map(function(item){return item})
   fun(entries[0].request, callback)
   entries.shift()
-  if (entries.length === 0) return
+  if (entries.length === 0) {
+    return
+  }
   oneByOne(entries, fun, callback)  
 }
 
@@ -38,6 +28,14 @@ function allAtOnce(arr, fun, callback) {
   })
 }
 
-function cloneArray(arr) {
-  return arr.map(function(item){return item})
+module.exports = {
+  replayOne: function(har, index, callback) {
+    replay(har.log.entries[index].request, callback)
+  }, 
+  replayAll: function(har, callback) {
+    oneByOne(har.log.entries, replay, callback)
+  }, 
+  replayAllInParallel: function(har, callback) {
+    allAtOnce(har.log.entries, replay, callback)
+  }
 }
